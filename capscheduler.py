b11c8a34cafdd813e967184c11812e00a797e5bb
John Madon <john@unwisegeek.net>
2021-03-15 14:41:17 -0400

    Added code comments and fixed date bug.

--- capscheduler.py
+++ capscheduler.py
@@ -12,25 +12,35 @@ DAYNUM = { 'Monday': 0,
                'Saturday': 5,
                'Sunday': 6,
 }
+
+DATEFMT = '%m-%d-%Y'
     
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    nextMeetingDay = date.today()
-    while nextMeetingDay.weekday() != DAYNUM['Thursday']:
-        nextMeetingDay += timedelta(1)
-    meetingDate = nextMeetingDay.stftime("%Y-%m-%d")
-    return redirect('/schedule?meetingDate={}'.format(meetingDate))
+    # If the user arrives at the index page without a date, pick today.
+    if 'meetingDate' in request.values: # Date exists in POST or GET
+        meetingDate = request.values.get('meetingDate')
+        return redirect('/schedule?meetingDate={}'.format(meetingDate))
+    else: # Pick the next day that coincides with the meeting day.
+        nextMeetingDay = date.today()
+        while nextMeetingDay.weekday() != DAYNUM['Thursday']:
+            nextMeetingDay += timedelta(1)
+        meetingDate = nextMeetingDay.strftime(DATEFMT)
+        return redirect('/schedule?meetingDate={}'.format(meetingDate))
 
 @app.route('/schedule', methods=['GET', 'POST'])
 def schedule_window():
     if "meetingDate" in request.values:
         meetingDate = request.values.get('meetingDate')
-        prevDate = meetingDate.strptime("%Y-%m-%d") + timedelta(-7)
-        prevDate = prevDate.strftime("%Y-%m-%d")
-        nextDate = meetingDate.strptime("%Y-%m-%d") + timedelta(+7)
-        nextDate = nextDate.strftime("%Y-%m-%d")
+        meetingDate = datetime.strptime(meetingDate, DATEFMT)
+        prevDate = meetingDate + timedelta(-7)
+        prevDate = prevDate.strftime(DATEFMT)
+        nextDate = meetingDate + timedelta(+7)
+        nextDate = nextDate.strftime(DATEFMT)
+        meetingDate = meetingDate.strftime('%Y-%m-%d')
         return render_template('index.html', meetingDate=meetingDate, prevDate=prevDate, nextDate=nextDate)
     else:
+        # Return an error if the date has not been set.
         return '', 400
 
 if __name__ == '__main__':
