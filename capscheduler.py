66c7c3491444957b3a5177a029b7ca00aade3b7a
John Madon <john@unwisegeek.net>
2021-03-15 22:39:42 -0400

    Fixed typos and added event views.

--- capscheduler.py
+++ capscheduler.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///capscheduler.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 DAYNUM = { 'Monday': 0,
@@ -16,7 +17,7 @@ DAYNUM = { 'Monday': 0,
                'Sunday': 6,
 }
 
-DATEFMT = '%m-%d-%Y'
+DATEFMT = '%Y-%m-%d'
 meetingDay = 'Thursday'
 
 # Create the event model for the database.
@@ -33,7 +34,7 @@ class Event(db.Model):
     isEmailScheduled = db.Column(db.Integer, unique=False, nullable=False)
     isEmailSent = db.Column(db.Integer, unique=False, nullable=False)
     isEmailConfirmed = db.Column(db.Integer, unique=False, nullable=False)
-    isEventDeleted = db.Column(db.Integer, unique=False, nullable=False)
+    isDeleted = db.Column(db.Integer, unique=False, nullable=False)
 
 # Initialize the database file if one does not exist.
 if not os.path.exists('capscheduler.db'):
@@ -57,48 +58,70 @@ def index():
 def schedule_window():
     if "meetingDate" in request.values:
         meetingDate = request.values.get('meetingDate')
-        meetingDate = datetime.strptime(meetingDate, DATEFMT)
+        # Account for meetingDate coming back in a different format
+        if meetingDate[3] == '-': # Month First
+            meetingDate = datetime.strptime(meetingDate, DATEFMT)
+        else: # Year First
+            meetingDate = datetime.strptime(meetingDate, '%Y-%m-%d')
         prevDate = meetingDate + timedelta(-7)
         prevDate = prevDate.strftime(DATEFMT)
         nextDate = meetingDate + timedelta(+7)
         nextDate = nextDate.strftime(DATEFMT)
         meetingDate = meetingDate.strftime('%Y-%m-%d')
-        return render_template('index.html', meetingDate=meetingDate, prevDate=prevDate, nextDate=nextDate)
+        # Get results from DB
+        queryResults = Event.query.filter_by(eventDate=meetingDate)
+        sortedQueryResults = []
+        counter = 0
+        while True:
+            try:
+                a = queryResults[counter].eventId
+                counter += 1
+            except:
+                break
+        for i in range(0, counter):
+            if queryResults[i].isDeleted != 1:
+                row = "{}|{}|{}|{}|{}|{}|{}|{}|{}|{}".format(queryResults[i].eventId, queryResults[i].eventDate, queryResults[i].startTime, \
+                                                             queryResults[i].stopTime, queryResults[i].eventName, queryResults[i].eventLdr, \
+                                                             queryResults[i].isAgreedTo, queryResults[i].isEmailScheduled, queryResults[i].isEmailSent, \
+                                                             queryResults[i].isEmailConfirmed)
+                sortedQueryResults += [ row.split('|') ]
+        return render_template('index.html', meetingDate=meetingDate, prevDate=prevDate, nextDate=nextDate, results=sortedQueryResults)
     else:
         # Redirect if the date has not been set.
-        redirect('/')
+        return redirect('/')
 
 @app.route('/newevent', methods=['GET', 'POST'])
 def newevent():
     # Check that all variables that are needed to create an event in the database are there.
     allVarsExist = True
-    for each in [ "eventDate", "startTime", "stopTime", "eventName", "eventLdr", "isConfirmed", "isEmailScheduled", "isEmailSent", "isEmailConfirmed" ]:
+    for each in [ "eventDate", "startTime", "stopTime", "eventName", "eventLdr", "isAgreedTo", "isEmailScheduled", "isEmailSent", "isEmailConfirmed" ]:
         if each not in request.values:
             allVarsExist = False
     if allVarsExist:
         # Create list of variable values.
-        data[0] = request.values.get('eventDate')
-        data[1] = request.values.get('startTime')
-        data[2] = request.values.get('stopTime')
-        data[3] = request.values.get('eventName')
-        data[4] = request.values.get('eventLdr')
-        data[5] = request.values.get('isAgreedTo')
-        data[6] = request.values.get('isEmailScheduled')
-        data[7] = request.values.get('isEmailSent')
-        data[8] = request.values.get('isEmailConfirmed')
-        data[9] = request.values.get('isDeleted')
+        data = []
+        data += [ request.values.get('eventDate') ]
+        data += [ request.values.get('startTime') ]
+        data += [ request.values.get('stopTime') ]
+        data += [ request.values.get('eventName') ]
+        data += [ request.values.get('eventLdr') ]
+        data += [ request.values.get('isAgreedTo') ]
+        data += [ request.values.get('isEmailScheduled') ]
+        data += [ request.values.get('isEmailSent') ]
+        data += [ request.values.get('isEmailConfirmed') ]
+        data += [ request.values.get('isDeleted') ]
         # Create a new DB entry.
-        newevent = Event(eventDate=data[0], startTime=data[1], stopTime=data[2], eventName=data[3], eventLdr=data[5], \
-                         isAgreedTo=data[6], isEmailScheduled=data[7], isEmailSent=data[8], isEmailConfirmed=data[8], \
-                         isDeleted=data[9])
+        newevent = Event(eventDate=data[0], startTime=data[1], stopTime=data[2], eventName=data[3], eventLdr=data[4], \
+                         isAgreedTo=data[5], isEmailScheduled=data[6], isEmailSent=data[7], isEmailConfirmed=data[8], \
+                         isDeleted=9)
         # Commit the DB entry and send them back to the index page with the previous date.
         db.session.add(newevent)
         db.session.commit()
         newevent = ""
         # Redirect
         meetingDate = request.values.get('meetingDate')
-        redirect('/schedule?meetingDate={}'.format(meetingDate))
-
+        return redirect('/schedule?meetingDate={}'.format(meetingDate))
+    return ""
 
 
 if __name__ == '__main__':
