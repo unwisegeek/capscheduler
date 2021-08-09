08f9052ad29a608032803f8309035ce30fa0fdc5
John Madon <john@unwisegeek.net>
2021-08-09 02:34:39 -0400

    black and flake8 code formatting and linting

--- const.py
+++ const.py
@@ -1,52 +1,53 @@
-DAYNUM = { 'Monday': 0,
-               'Tuesday': 1,
-               'Wednesday': 2,
-               'Thursday': 3,
-               'Friday': 4,
-               'Saturday': 5,
-               'Sunday': 6,
+DAYNUM = {
+    "Monday": 0,
+    "Tuesday": 1,
+    "Wednesday": 2,
+    "Thursday": 3,
+    "Friday": 4,
+    "Saturday": 5,
+    "Sunday": 6,
 }
 
 MONTHNUM = {
-    1: 'January',
-    2: 'February',
-    3: 'March',
-    4: 'April',
-    5: 'May',
-    6: 'June',
-    7: 'July',
-    8: 'August',
-    9: 'September',
-    10: 'October',
-    11: 'November',
-    12: 'December'
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
 }
 
 CONTACT_ACCOUNTS = [
-    'None',
-    'Aerospace',
-    'Character Development',
-    'Emergency Services',
-    'Leadership',
-    'Physical Training',
-    'Safety'
+    "None",
+    "Aerospace",
+    "Character Development",
+    "Emergency Services",
+    "Leadership",
+    "Physical Training",
+    "Safety",
 ]
 
 CONTACT_ABRVS = {
-    'None': 'NA',
-    'Aerospace': 'AE',
-    'Character Development': 'CD',
-    'Emergency Services': 'ES',
-    'Leadership': 'LD',
-    'Physical Training': 'PT',
-    'Safety': 'SF'
+    "None": "NA",
+    "Aerospace": "AE",
+    "Character Development": "CD",
+    "Emergency Services": "ES",
+    "Leadership": "LD",
+    "Physical Training": "PT",
+    "Safety": "SF",
 }
 
 CONTACT_ACCT_REQS = {
-    'Aerospace': 90,
-    'Character Development': 60,
-    'Leadership': 90,
-    'Physical Training': 60
+    "Aerospace": 90,
+    "Character Development": 60,
+    "Leadership": 90,
+    "Physical Training": 60,
 }
 
-DATEFMT = '%Y-%m-%d'
+DATEFMT = "%Y-%m-%d"
