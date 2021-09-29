66f1741a02e00eb1e9c0f8a5fa5f4b9c17874583
John Madon <john@unwisegeek.net>
2021-09-29 03:38:45 -0400

    added Google login to blueprint registration

--- capscheduler.py
+++ capscheduler.py
@@ -269,7 +269,7 @@ GOOGLE_BLUEPRINT = make_google_blueprint(
     hosted_domain="mariettacap.org",
 )
 
-app.register_blueprint(GOOGLE_BLUEPRINT, url_prefix="/login")
+app.register_blueprint(GOOGLE_BLUEPRINT, url_prefix="/google/login")
 login_manager.init_app(app)
 
 
